ce559344fc67a5854a0c2887e7d8984e01dbc640
Pogozhelskaya Vlada <pogozhelskaya@gmail.com>
2020-09-24 06:34:25 +0300

    Report is added

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import argparse
+import timeit
+from statistics import fmean, variance
 
 from src.label_graph import LabelGraph
-from src.rpq import rpq
+from src.rpq import rpq, rpq_with_linear_tc
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='command line interface for simple graph database')
+    parser = argparse.ArgumentParser(
+        description='command line interface for simple graph database')
     parser.add_argument(
         '--graph'
         , required=True
@@ -34,8 +37,27 @@ if __name__ == '__main__':
     g = LabelGraph.from_txt(args.graph)
     r = LabelGraph.from_regex(args.regex)
 
-    res = rpq(g, r)
+    print(str(args.graph) + " " + str(args.regex))
 
+    time_sum_1 = 0
+    time_sum_2 = 0
+
+    for i in range(5):
+        time_1 = timeit.default_timer()
+        res_1 = rpq(g, r)
+        time_sum_1 += timeit.default_timer() - time_1
+
+        time_2 = timeit.default_timer()
+        res_2 = rpq_with_linear_tc(g, r)
+        time_sum_2 += timeit.default_timer() - time_2
+
+        assert (res_1.nvals == res_2.nvals)
+        print(str(res_1.nvals))
+
+    print(str(time_sum_1 / 5))
+    print(str(time_sum_2 / 5))
+
+    res = rpq(g, r)
     srcs = None
     if args.sources is not None:
         with open(args.sources, 'r') as f:
@@ -45,8 +67,12 @@ if __name__ == '__main__':
     if args.destinations is not None:
         with open(args.destinations, 'r') as f:
             dsts = list(map(int, f.readline().split()))
-
+    f = open("output.txt", 'a')
+    f.write(str(args.graph) + " " + str(args.regex) + "\n")
+    start_time = timeit.default_timer()
     for i, j, _ in zip(*res.to_lists()):
         if (srcs is None) or (i in srcs):
             if (dsts is None) or (j in dsts):
-                print(f'There is path from {i} to {j} in graph')
+                f.write(f'{i} to {j}')
+    print(str(timeit.default_timer() - start_time))
+    f.close()
---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+REGEX=refinedDataForRPQ/LUBM300/regexes/'q*0'
+GRAPHS=("refinedDataForRPQ/LUBM300/LUBM300.txt" "refinedDataForRPQ/LUBM500/LUBM500.txt" "refinedDataForRPQ/LUBM1M/LUBM1M.txt" "refinedDataForRPQ/LUBM1.5M/LUBM1.5M.txt" "refinedDataForRPQ/LUBM1.9M/LUBM1.9M.txt")
+
+for (( i=0; i < 5; i++ ))
+do
+    for r in $REGEX
+    do
+        python3 main.py --graph ${GRAPHS[i]} --regex $r
+    done
+done
